36dac6d98e19e5c63997e908583f9f22f743da83
obold <p240188@gmail.com>
2023-10-22 17:45:57 +0500

    настройка кнопки кторая сворачивает сайдбар
    https://youtu.be/UA8Sxk_mHb8?t=1667

--- src/layout/Sidebar/Sidebar.jsx
+++ src/layout/Sidebar/Sidebar.jsx
@@ -1,17 +1,26 @@
 import "./Sidebar.css"
 import { personsImgs } from '../../utils/images'
 import { navigationLinks } from '../../data/data'
-import { useContext, useState } from 'react'
+import { useContext, useEffect, useState } from 'react'
 import { SidebarContext } from '../../context/SidebarContext'
 
 const Sidebar = () => {
 
   const [activeLinkIdx] = useState(1);
   const [sidebarClass, setSidebarClass] = useState("");
-  const { isSidebarOpen } = useContext(SidebarContext)
+  const { isSidebarOpen } = useContext(SidebarContext);
+
+  useEffect(() => {
+    if(isSidebarOpen){
+      setSidebarClass("sidebar-change")
+    } else {
+      setSidebarClass('')
+    }
+  }, [isSidebarOpen])
+
 
   return (
-    <div className={'sidebar'}>
+    <div className={`sidebar ${sidebarClass}`}>
       <div className="user-info">
         <div className="info-img img-fit-cover">
           <img src={personsImgs.person_two} alt="profile image"/>
