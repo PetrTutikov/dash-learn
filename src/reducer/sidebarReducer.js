74c6a46e3118fe61c49ef6c112f10bb880ebf775
obold <p240188@gmail.com>
2023-10-22 11:39:53 +0500

    загрузил локально и реформатировал шрифт в woff2 подключил через @font-face по уроку
    https://youtu.be/qxJEcHjk8Xs?list=PL0MUAHwery4o9I7QQVj_RP4ZVpmdx6evz&t=665

---
+++ src/reducer/sidebarReducer.js
@@ -0,0 +1,11 @@
+const sidebarReducer = (state, action) => {
+  switch (action.type) {
+    case "TOGGLE_SIDEBAR":
+      return {...state}
+
+
+
+    default:
+      return state, isSidbar;
+  }
+}
