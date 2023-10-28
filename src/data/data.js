23717418e9344de7c1ec99258d5da1ebd8d71cd9
obold <p240188@gmail.com>
2023-10-28 17:11:48 +0500

    правки меню
    https://youtu.be/UA8Sxk_mHb8?t=5037

--- src/data/data.js
+++ src/data/data.js
@@ -2,16 +2,12 @@ import { iconsImgs } from "../utils/images";
 import { personsImgs } from "../utils/images";
 
 export const navigationLinks = [
-    { id: 1, title: 'Home', image: iconsImgs.home },
-    { id: 2, title: 'Budget', image: iconsImgs.budget },
-    { id: 3, title: 'Transactions', image: iconsImgs.plane },
-    { id: 4, title: 'Subscriptions', image: iconsImgs.wallet },
-    { id: 5, title: 'Loans', image: iconsImgs.bills },
-    { id: 6, title: 'Reports', image: iconsImgs.report },
-    { id: 7, title: 'Savings', image: iconsImgs.wallet },
-    { id: 8, title: 'Financial Advice', image: iconsImgs.wealth },
-    { id: 9, title: 'Account', image: iconsImgs.user },
-    { id: 10, title: 'Settings', image: iconsImgs.gears }
+    { id: 1, title: 'Рабочий стол', image: iconsImgs.report },
+    { id: 2, title: 'Клиенты', image: iconsImgs.user },
+    { id: 3, title: 'Объекты', image: iconsImgs.home },
+    { id: 4, title: 'Документы', image: iconsImgs.budget },
+    { id: 5, title: 'Финансы', image: iconsImgs.bills },
+    { id: 10, title: 'Настройки', image: iconsImgs.gears }
 ];
 
 export const transactions = [
