864fd9aebb7e2cc069b8d1ae717c3549ec9e2462
obold <p240188@gmail.com>
2023-10-28 16:46:04 +0500

    Звершение проекта
    https://youtu.be/UA8Sxk_mHb8?t=5037

--- src/components/Financial/Financial.jsx
+++ src/components/Financial/Financial.jsx
@@ -1,9 +1,22 @@
 import "./Financial.css"
+import { iconsImgs } from '../../utils/images'
 
 const Financial = () => {
   return (
     <div>
-
+      <div className="subgrid-two-item grid-common grid-c8">
+        <div className="grid-c-title">
+          <h3 className="grid-c-title-text"> Financial Advice</h3>
+          <button className="grid-c-title-icon" type="button">
+            <img src={iconsImgs.plus}/>
+          </button>
+        </div>
+        <div className="grid-c8-content">
+          <p className="text-silver-v1">
+            Ipsum dolor sit amet consectetur elit. Iste, vitae ...
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
